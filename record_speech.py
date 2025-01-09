8eee68a8aefb4ad14e4939ab5c703ce36a80bef4
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-01-09 19:19:05 +0000

    Update record_speech.py

--- record_speech.py
+++ record_speech.py
@@ -30,7 +30,7 @@ def record_speech():
     # device index on this system
     device_index = 1
     # 44100 samples per second
-    sample_rate = 44100
+    sample_rate = 48000
     record_seconds = 10
     # initialize PyAudio object
     p = pyaudio.PyAudio()
