35496494ffe63df5196e161bb85317a32855a094
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-01-10 14:03:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from dotenv import load_dotenv
+import os
 import record_speech;
+
+# Load the .env file
+load_dotenv()
+
+# Access the environment variables
+openai_api_key        = os.getenv("OPENAI_API_KEY")
+elevenlabs_api_key    = os.getenv("ELEVENLABS_API_KEY")
+elevenlabs_voice_1_id = os.getenv("ELEVENLABS_VOICE_1_ID")
 
 print("Starting...");
 record_speech.record_speech();
