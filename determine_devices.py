0416b914e52936c01556d0aeb73ca744edb17cfd
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-01-10 17:54:23 +0000

    Update determine_devices.py

--- determine_devices.py
+++ determine_devices.py
@@ -4,3 +4,6 @@ p = pyaudio.PyAudio()
 for i in range(p.get_device_count()):
     info = p.get_device_info_by_index(i)
     print(f"Device index {i}: {info['name']}")
+    print(f"  Max input channels : {info['maxInputChannels']}")
+    print(f"  Max output channels: {info['maxOutputChannels']}")
+    print("------")
