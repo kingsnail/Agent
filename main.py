0462c5c786e515f506c2e43c581b7e73998901f1
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-01-10 14:10:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from dotenv import load_dotenv
 import os
-import record_speech;
+import record_speech
+import picovoice
 
 # Load the .env file
 load_dotenv()
 
 # Access the environment variables
 openai_api_key        = os.getenv("OPENAI_API_KEY")
+elevenlabs_user       = os.getenv("ELEVENLABS_USER")
 elevenlabs_api_key    = os.getenv("ELEVENLABS_API_KEY")
 elevenlabs_voice_1_id = os.getenv("ELEVENLABS_VOICE_1_ID")
+porcupine_access_key  = os.getenv("PORCUPINE_ACCESS_KEY")
 
 print("Starting...");
-record_speech.record_speech();
+#record_speech.record_speech();
 print("Complete.");
