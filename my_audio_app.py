17e4733c6031adfe2ee7caad62fb5a2246d237d8
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-01-11 13:05:13 +0000

    Update my_audio_app.py

--- my_audio_app.py
+++ my_audio_app.py
@@ -1,4 +1,7 @@
-from dotenv import load_dotenv
+from   dotenv import load_dotenv
 import os
 import base64
 import asyncio
+from   openai import AsyncOpenAI
+from   openai.types.beta.realtime.session import Session
+from   openai.resources.beta.realtime.realtime import AsyncRealtimeConnection
