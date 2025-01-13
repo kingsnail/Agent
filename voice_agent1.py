7688a08a47ca623f8b0accf4880236fc1fa392d3
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-01-13 20:13:46 +0000

    Update voice_agent1.py

--- voice_agent1.py
+++ voice_agent1.py
@@ -175,8 +175,8 @@ try:
                     ) as response:
                         print(f"Time to first byte: {int((time.time() - start_time) * 1000)}ms")
                         for chunk in response.iter_bytes(chunk_size=2048):
-                        #print("chunk")
-                        player_stream.write(chunk)
+                            #print("chunk")
+                            player_stream.write(chunk)
 
                 print(f"Done in {int((time.time() - start_time) * 1000)}ms.")
 
